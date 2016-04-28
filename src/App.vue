5e925aa63c1589dd64c7a80d45bdb7ab9d2bfce7
William Justice <williamjustice@Williams-MacBook-Air.local>
2016-04-28 08:02:41 -0600

    copy to clipboard added

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,68 @@
 
     <main class="wrapper">
       <div class="content">
-        <h1>Oh hi there <span class="emoji"><img src="https://twemoji.maxcdn.com/svg/1f44b.svg"></span>! My name is Will and these days I'm a <a href="http://bradfrost.com/blog/post/frontend-design/" target="_black">front-end designer</a> (but over the years I've worked as a UX/UI designer and full-stack developer, respectively). I know, I know, there isn't much here. Most of my work is NDA protected (<span class="emoji"><img src="https://twemoji.maxcdn.com/svg/1f611.svg"></span>). But if you're interested (!) please reach out and I can probably send you some cool stuff <span class="emoji"><img src="https://twemoji.maxcdn.com/svg/1f44d.svg"></span>.</h1>
+        <h1>Oh hi there <span class="emoji"><img src="https://twemoji.maxcdn.com/svg/1f44b.svg"></span>! My name is Will Justice and these days I'm a <a class="no-wrap" href="http://bradfrost.com/blog/post/frontend-design/" target="_black">front-end designer</a> (but over the years I've worked as a UX/UI designer and full-stack developer, respectively). I know, I know, there isn't much here. Most of my work is NDA protected (<span class="emoji"><img src="https://twemoji.maxcdn.com/svg/1f611.svg"></span>). But if you're interested (!) please reach out and I can probably send you some cool stuff <span class="emoji"><img src="https://twemoji.maxcdn.com/svg/1f44d.svg"></span>.</h1>
 
         <div class="contact-links">
-          <a href="mailto:will@thewilljustice.com" target="_top" @click="copied()">will@thewilljustice.com</a>
-          <a href="https://www.linkedin.com/in/will-justice-aa524857" target="_blank">LinkedIn</a>
-          <a href="https://github.com/wjustice" target="_blank">Github</a>
+          <div class="email-wrapper">
+            <a class="link" href="mailto:will@thewilljustice.com?Subject=Thanks%20for%20the%20awesome%20subject%20line" id="email">will@thewilljustice.com</a>
+            <div v-if="showTooltip" class="tooltip" transition="fade">Copied to clipboard</div>
+            <button class="btn" data-clipboard-target="#email"><img src="./assets/clippy.svg"></button>
+          </div>
+          <a class="link" href="https://www.linkedin.com/in/will-justice-aa524857" target="_blank">LinkedIn</a>
+          <a class="link" href="https://github.com/wjustice" target="_blank">Github</a>
         </div>
 
       </div>
     </main>
 
-  </div>
+  </div>  
 </template>
 
 <script>
+const Clipboard = require('clipboard');
 export default {
   data () {
     return {
-      
+        showTooltip: false
     }
   },
   methods: {
-    copied: function () {
-      console.log('copied');
+    toggleTooltip: function () {
+      console.log(this.showTooltip);
+      this.showTooltip = !this.showTooltip;
     }
+  },
+  created: function () {
+    let toggle = this.toggleTooltip;
+    let clipboard = new Clipboard('.email-wrapper .btn')
+    clipboard.on('success', function(e) {
+      e.clearSelection();
+      toggle()
+      setTimeout(function () {
+        toggle()
+      }, 1000)
+    });
   }
 }
 </script>
 
 <style lang="sass">
-  // @import './scss/index.scss';
-  // Thumbs-up: 1f44d
-  // Wave: 1f44b
+  @import './scss/main.scss';
   $red: #D03802;
   $blue: #008CCE;
   $background: #171D22;
+
+  ::-moz-selection {
+    color: white;
+    background: pink;
+  }
+
+  ::selection {
+      color: white; 
+      background: pink;
+  }
+
   body{
     background: #171D22;
     color: #008CCE;
@@ -49,11 +75,15 @@ export default {
     font-size: 1.6em;
     font-weight: 100;
   }
-  a {
+  a, .link {
     color: #888888;
     font-weight: 100;
     position: relative;
     text-decoration: none;
+  }
+  a.no-wrap{
+    display: inline-block;
+    white-space: normal;
   }
   span img{
     width: 1em;
@@ -75,60 +105,40 @@ export default {
     display: flex;
     justify-content: center;
   }
-  .contact-links{
-    width: 100%;
-    display: flex;
-    justify-content: left;
-    // padding: 10px;
-    // border-top: 2px solid $red;
-    // border-bottom: 2px solid $red;
+  .email-wrapper{
+    position: relative;
+    margin-right: 1.7em;
   }
-  .contact-links a{
-    font-size: 1.6em;
-    margin-right: 1em;
+  .email-wrapper .btn {
+    cursor: pointer;
+    z-index: 1001;
+    background: none;
+    border: none;
+    position: absolute;
+    right:-.4em;
+    top: .4em;
   }
 
-  .content a {
-  padding: 0 0.25em;
-  font-weight: 500;
-  -webkit-transition: color 0.2s;
-  transition: color 0.2s;
-}
-
-.content a:hover {
-  color: #d04c3f;
-}
-
-.content a::before,
-.content a::after {
-  position: absolute;
-  top: -0.1em;
-  font-weight: 100;
-  font-size: 150%;
-  line-height: 1;
-  opacity: 0;
-  -webkit-transition: opacity 0.2s, -webkit-transform 0.2s;
-  transition: opacity 0.2s, transform 0.2s;
-}
-
-.content a::before {
-  left: -0.23em;
-  content: '[';
-  -webkit-transform: translateX(-100%);
-  transform: translateX(-100%);
-}
-
-.content a::after {
-  right: -0.23em;
-  content: ']';
-  -webkit-transform: translateX(100%);
-  transform: translateX(100%);
-}
-
-.content a:hover::before,
-.content a:hover::after {
-  opacity: 1;
-  -webkit-transform: translateX(0);
-  transform: translateX(0);
-}
+  .email-wrapper .btn:focus{
+    border: none;
+    outline: none;
+  }
+  .email-wrapper .btn img {
+    width: 1.8em;
+  }
+  .tooltip {
+    position: absolute;
+    background: white;
+    padding: 0 5px 0 5px;
+    border-radius: 3px;
+    font-size: .8em;
+    right: 2.4em;
+    top: -1.3em;
+  }
+  .fade-transition {
+    transition: opacity ease .2s;
+  }
+  .fade-enter, .fade-leave{
+    opacity: 0;
+  }
 </style>
